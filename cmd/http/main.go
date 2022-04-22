64c72f816616780e156705e8833756fcdbbb639a
Kazuki Onishi <princeontrojanhorse@gmail.com>
2022-04-22 23:14:25 +0900

    :sparkles: add cli library

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -3,22 +3,52 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"strings"
+
+	hh "github.com/0n1shi/http-honeypot"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	http.HandleFunc("/", handle)
-	http.ListenAndServe(":8080", nil)
+	(&cli.App{
+		Name:  "http honeypot",
+		Usage: "http server working as honeypot",
+		Commands: []*cli.Command{
+			{
+				Name:    "serve",
+				Aliases: []string{"s"},
+				Usage:   "start honeyport http server",
+				Action:  serve,
+			},
+		},
+	}).Run(os.Args)
+
 }
 
-func handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("%s %s %s\n", r.Method, r.RequestURI, r.Proto)
+func serve(c *cli.Context) error {
+	log.SetFlags(log.Llongfile | log.Ldate | log.Ltime)
+	log.SetPrefix("[http honeypot]")
 
-	for k, v := range r.Header {
-		fmt.Printf("%s: %s\n", k, strings.Join(v, " "))
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("received a http request")
+		req := hh.Request{}
+		fmt.Printf("%s %s %s\n", r.Method, r.RequestURI, r.Proto)
+		req.Method = r.Method
+		req.URL = r.RequestURI
+		req.Proto = r.Proto
+		for k, v := range r.Header {
+			val := strings.Join(v, " ")
+			fmt.Printf("%s: %s\n", k, val)
+			req.Header[k] = val
+		}
+		body, _ := ioutil.ReadAll(r.Body)
+		fmt.Printf("\n%s\n", string(body))
+		req.Body = string(body)
+	})
 
-	body, _ := ioutil.ReadAll(r.Body)
-	fmt.Printf("\n%s\n", string(body))
+	http.ListenAndServe(":8080", nil)
+	return nil
 }
--- model.go
+++ model.go
@@ -1,9 +1,9 @@
 package httphoneypot
 
 type Request struct {
-	Method string              `json:"method"`
-	URL    string              `json:"url"`
-	Proto  string              `json:"version"`
-	Header map[string][]string `json:"headers"`
-	Body   string              `json:"body"`
+	Method string            `json:"method"`
+	URL    string            `json:"url"`
+	Proto  string            `json:"version"`
+	Header map[string]string `json:"headers"`
+	Body   string            `json:"body"`
 }
